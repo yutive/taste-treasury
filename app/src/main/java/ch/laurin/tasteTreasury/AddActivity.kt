0f191aceb9a74775dab520d03884856e6b233f18
laurin <laurin.schwander@outlook.com>
2024-01-19 13:28:38 +0100

    made a CameraActivity

--- app/src/main/java/ch/laurin/tasteTreasury/AddActivity.kt
+++ app/src/main/java/ch/laurin/tasteTreasury/AddActivity.kt
@@ -1,14 +1,17 @@
 package ch.laurin.tasteTreasury
 
+import android.Manifest
 import android.app.Activity
 import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
 import android.os.VibrationEffect
 import android.os.Vibrator
-import android.os.VibratorManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -30,6 +33,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
+import androidx.core.content.ContextCompat
 import ch.laurin.tasteTreasury.data.Recipe
 import ch.laurin.tasteTreasury.ui.recipe_list.RecipeListViewModel
 import ch.laurin.tasteTreasury.ui.theme.TastetreasuryTheme
@@ -89,6 +93,29 @@ fun RecipeFormWithButton(recipeViewModel: RecipeListViewModel) {
             label = { Text("Description") },
             modifier = Modifier.fillMaxWidth()
         )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Button(
+            onClick = {
+                coroutineScope.launch {
+                    recipeViewModel.addRecipe(
+                        Recipe(
+                            name = nameText,
+                            description = descriptionText,
+                        )
+                    )
+                    val timing = 50L
+                    vibrator.vibrate(VibrationEffect.createOneShot(timing, VibrationEffect.DEFAULT_AMPLITUDE))
+                    context.startActivity(Intent(context, CameraActivity::class.java))
+                }
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(50.dp)
+        ) {
+            Text(text = "Take Picture", color = MaterialTheme.colorScheme.onPrimary)
+        }
 
         Spacer(modifier = Modifier.height(16.dp))
 
---
+++ app/src/main/java/ch/laurin/tasteTreasury/CameraActivity.kt
@@ -0,0 +1,68 @@
+package ch.laurin.tasteTreasury
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.core.content.ContextCompat
+import ch.laurin.tasteTreasury.ui.theme.TastetreasuryTheme
+
+class CameraActivity : ComponentActivity() {
+
+    private val cameraPermissionRequest =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+            if (isGranted) {
+                // Implement camera related  code
+            } else {
+                // Camera permission denied (Handle denied operation)
+            }
+
+        }
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        when (PackageManager.PERMISSION_GRANTED) {
+            ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.CAMERA
+            ) -> {
+                // Camera permission already granted
+                // Implement camera related code
+            }
+            else -> {
+                cameraPermissionRequest.launch(Manifest.permission.CAMERA)
+            }
+        }
+
+
+
+        setContent {
+            TastetreasuryTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
