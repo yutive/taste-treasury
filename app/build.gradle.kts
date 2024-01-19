80ee533ceb03a2b5c97942010d4657d918b9f144
laurin <laurin.schwander@outlook.com>
2024-01-19 13:40:30 +0100

    imported camera implementation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,7 +50,11 @@ android {
 }
 
 dependencies {
-
+    val cameraxVersion = "1.3.0"
+    implementation ("androidx.camera:camera-core:${cameraxVersion}")
+    implementation ("androidx.camera:camera-camera2:${cameraxVersion}")
+    implementation ("androidx.camera:camera-view:${cameraxVersion}")
+    implementation ("androidx.camera:camera-lifecycle:$cameraxVersion")
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.activity:activity-compose:1.8.2")
